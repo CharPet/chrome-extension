dcf597112e76adbf82de3e2e2c6ad3752ed8f999
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-23 16:53:59 +0200

    Added exercises with innerHTML

--- index.js
+++ index.js
@@ -4,8 +4,36 @@
 
 // -----------------------------------
 
-let inputBtn = document.getElementById("input-btn");
+// const inputBtn = document.getElementById("input-btn");
+
+// inputBtn.addEventListener("click", function () {
+//   console.log("Button clicked from addEventListener");
+// });
+
+// -----------------------------------
+
+// const myLeads = [];
+// const inputEl = document.getElementById("input-el");
+
+// -----------------------------------
+
+const myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+// console.log(ulEl);
 
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked from addEventListener");
+  // myLeads.push("www.awesomelead.com");
+  myLeads.push(inputEl.value);
+  // https://www.w3schools.com/jsreF/prop_text_value.asp
+  console.log(myLeads);
+
+  for (let i = 0; i < myLeads.length; i++) {
+    // console.log(myLeads[i]);
+    // ulEl.textContent += myLeads[i] + " ";
+    ulEl.innerHTML = "<li>" + myLeads[i] + "</li>";
+  }
 });
--- index2.js
+++ index2.js
@@ -3,3 +3,39 @@ let box = document.getElementById("box");
 box.addEventListener("click", function () {
   console.log("I want to open the box");
 });
+
+// --------------------------------------
+
+// If possible, use const. If not, use let.
+
+// Which variables below should be changed from let to const?
+
+// The customer wants to order some stuff. Here are the details:
+const basePrice = 520;
+const discount = 120;
+let shippingCost = 12;
+let shippingTime = "5-12 days";
+
+// Whops! Turns out the shipping will be a bit more complex
+shippingCost = 15;
+shippingTime = "7-14 days";
+
+// Calculating the full price
+const fullPrice = basePrice - discount + shippingCost;
+
+// Finally, notifying the customer
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime);
+
+// --------------------------------------
+
+const container = document.getElementById("container");
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>";
+
+// --------------------------------------
+
+function buy() {
+  container.innerHTML += "<p>Thank you for buying!</p>";
+}
+
+// --------------------------------------
