917ec554382047a8d97e980fc32c48f6e503d16a
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-24 16:47:03 +0200

    Added 2 rendering functions and modified li elements by using template strings

--- index.js
+++ index.js
@@ -17,6 +17,11 @@
 
 // -----------------------------------
 
+// const myLeads = [
+//   "www.awesomelead.com",
+//   "www.epiclead.com",
+//   "www.greatlead.com",
+// ];
 const myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
@@ -25,15 +30,57 @@ const ulEl = document.getElementById("ul-el");
 // console.log(ulEl);
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from addEventListener");
+  // console.log("Button clicked from addEventListener");
   // myLeads.push("www.awesomelead.com");
   myLeads.push(inputEl.value);
   // https://www.w3schools.com/jsreF/prop_text_value.asp
-  console.log(myLeads);
+  // console.log(myLeads);
+
+  inputEl.value = "";
+
+  renderLeads();
+
+  // renderLead();
+  // inputEl.value = "";
+});
+
+function renderLeads() {
+  let listItems = "";
 
   for (let i = 0; i < myLeads.length; i++) {
     // console.log(myLeads[i]);
     // ulEl.textContent += myLeads[i] + " ";
-    ulEl.innerHTML = "<li>" + myLeads[i] + "</li>";
+
+    // ulEl.innerHTML = "<li>" + myLeads[i] + "</li>";
+    // OR
+    // 1. create element
+    // 2. set text contents
+    // 3. append to ul
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+
+    // listItems +=
+    //   "<a href='" +
+    //   myLeads[i] +
+    //   "' target='_blank'><li>" +
+    //   myLeads[i] +
+    //   "</li></a>";
+
+    listItems += `
+      <li>
+        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+      </li>
+    `;
+    console.log(listItems);
   }
-});
+
+  // ulEl.innerHTML = "<li>" + listItems + "</li>";
+  ulEl.innerHTML = listItems;
+}
+
+// faster function without iterating through the list every time a item is added
+// function renderLead() {
+//   let listItem = "<li>" + inputEl.value + "</li>";
+//   ulEl.innerHTML += listItem;
+// }
