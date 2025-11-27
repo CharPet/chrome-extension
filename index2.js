995609893b814995f49e937c51c6fb384d1b34ce
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-27 16:36:12 +0200

    Added exercise with truthy and falsy values

--- index2.js
+++ index2.js
@@ -56,3 +56,10 @@ const email = `Hey ${recipient}!
 console.log(email);
 
 // --------------------------------------
+
+console.log(Boolean("")); // falsy
+console.log(Boolean("0")); // truthy
+console.log(Boolean(100)); // truthy
+console.log(Boolean(null)); // falsy
+console.log(Boolean([0])); // truthy
+console.log(Boolean(-0)); // falsy
